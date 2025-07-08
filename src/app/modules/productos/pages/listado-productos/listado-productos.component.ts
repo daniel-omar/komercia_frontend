c08f209490fbce9cf7079d27f8ad64370d5dcf11
daniel-omar <omarmi15200@gmail.com>
2025-07-08 00:51:00 -0500

    -add redireccion crear producto

--- src/app/modules/productos/pages/listado-productos/listado-productos.component.ts
+++ src/app/modules/productos/pages/listado-productos/listado-productos.component.ts
@@ -7,13 +7,19 @@ import { ProductsFilter } from '../../interfaces/products-filter.interface';
 import { Observable } from 'rxjs';
 import { ProductsResponse } from '../../interfaces/products-response.interface';
 import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-listado-productos',
   imports: [
     CommonModule,
     FiltroProductosComponent,
-    TablaProductosComponent
+    TablaProductosComponent,
+
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [ProductsService],
   templateUrl: './listado-productos.component.html',
@@ -25,6 +31,8 @@ export class ListadoProductosComponent {
   public productsResponseObservable$!: Observable<ProductsResponse>;
 
   public paginador: Paginator = new Paginator({ per_page: 5, new_page: 1, total: 0, current_page: 0, pages: 0 });
+
+  private readonly router = inject(Router);
 
   ngOnInit() {
   }
@@ -39,4 +47,7 @@ export class ListadoProductosComponent {
     this.productsResponseObservable$ = this._productsService.getByFilters(this.productsFilter, this.paginador);
   }
 
+  goCreateProduct() {
+    this.router.navigate(["productos/nuevo-producto"]);
+  }
 }
